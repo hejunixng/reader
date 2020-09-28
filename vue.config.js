fb9a30c78c9f3a80c2a611e46a346c9ba3336b54
hejunxing <1063378763@qq.com>
2020-09-28 19:02:48 +0800

    env

--- vue.config.js
+++ vue.config.js
@@ -23,4 +23,11 @@ module.exports={
         // }
         
       },
+      configureWebpack: {
+        performance: {
+          hints: 'warning',
+          maxAssetSize: 524288 * 10,
+          maxEntrypointSize: 524288 * 10
+        }
+      }
 }
